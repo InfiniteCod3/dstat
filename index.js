30acb3c8ee21e114c8db798976ead72e68db7f85
InfiniteCod3 <foxyss@pm.me>
2024-04-10 18:12:45 +0200

    Update index.html and index.js files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express'),
 	app = express(),
 	http = require('http').createServer(app),
+	io = require('socket.io')(http),
 	path = require('path'),
 	fs = require('fs')
 var all_requests = 0,
@@ -9,23 +10,6 @@ var all_requests = 0,
 require('events').EventEmitter.defaultMaxListeners = Infinity
 
 app.use(express.static(path.join(__dirname, 'assets/')))
-
-app.get('/stats', (req, res) => {
-	res.header("Content-Type", "text/event-stream")
-	res.header("Cache-Control", "no-cache")
-	res.header("Connection", "keep-alive")
-
-	// Sending initial data
-	res.write(`data: ${per_requests}\n\n`)
-
-	// Setup interval to push updates
-	const interval = setInterval(() => {
-		res.write(`data: ${per_requests}\n\n`)
-	}, 1000)
-
-	// Clean up when connection is closed
-	req.on('close', () => clearInterval(interval))
-})
 
 app.get('/attack', (req, res) => {
 	all_requests++
@@ -40,9 +24,11 @@ setInterval(async() => {
 			max_requests: per_requests
 		}))
 	}
+
+	io.emit('requests', all_requests, per_requests, JSON.parse(config).max_requests)
 	per_requests = 0
 }, 1000)
 
 setInterval(() => all_requests = 0, 1000 * 86400)
 
-http.listen(8080)
+http.listen(80)
